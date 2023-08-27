b6c9871aba652c885aa201157a97fca41752edb7
Mohamed Elaqqad <elaqqadmohamed@gmail.com>
2023-08-27 22:37:33 +0200

    Find suspicious using engine analysis
    This a try to make sure we label all gambits correctly

--- scripts/download-gambits.py
+++ scripts/download-gambits.py
@@ -1,4 +1,5 @@
 from io import StringIO
+import os
 from time import sleep
 import chess.pgn
 import csv
@@ -6,11 +7,14 @@ import json
 import requests
 import re
 import urllib
+import chess
+import chess.engine
 
 
-CHESS_OPENINGS_URL = 'https://raw.githubusercontent.com/lichess-org/chess-openings/master'
-MASTERS_EXPLORER = 'https://explorer.lichess.ovh/masters?play='
-LICHESS_EXPLORER = 'https://explorer.lichess.ovh/lichess?variant=standard&speeds=blitz,rapid,classical&ratings=1800,2000,2200,2500&fen='
+CHESS_OPENINGS_URL = r'https://raw.githubusercontent.com/lichess-org/chess-openings/master'
+MASTERS_EXPLORER = r'https://explorer.lichess.ovh/masters?play='
+LICHESS_EXPLORER = r'https://explorer.lichess.ovh/lichess?variant=standard&speeds=blitz,rapid,classical&ratings=1800,2000,2200,2500&fen='
+STOCKFISH_PATH = r''
 OPENINGS_START = ['a', 'b', 'c', 'd', 'e']
 
 
@@ -24,7 +28,7 @@ def choose_top_game(games, winner):
 
 
 def make_api_call(url):
-    sleep(1)
+    # sleep(1)
     # print(url)
     headers = {
         'Accept': 'application/x-ndjson'
@@ -68,7 +72,7 @@ def is_gambit(name: str):
     return name.strip().lower().endswith('gambit')
 
 
-def analyse(pgn: str):
+def analyse(pgn: str, name: str, engine: chess.engine.SimpleEngine):
     split = re.split(r'\d*\.', pgn)
     game = chess.pgn.read_game(StringIO(pgn))
     while not game.is_end():
@@ -81,6 +85,10 @@ def analyse(pgn: str):
         (len(split)-1) if color_gambit == 'black' else 2*(len(split)-1)-1
     master_game = get_master_game(algebraic, color_gambit)
     lichess_game = get_lichess_game(fen, color_gambit)
+    if engine is not None :
+        result = engine.analyse(board, chess.engine.Limit(time=5))
+        if(result['score'].relative <= chess.engine.Cp(0)) :
+            print(f'Suspicious gambit {name} {result["score"]}, https://lichess.org/{lichess_game["game"]}/{color_gambit}#{move_number}, {fen}')
     return {
         'move': move_number,
         'color': color_gambit,
@@ -94,6 +102,12 @@ def analyse(pgn: str):
 
 
 if __name__ == '__main__':
+    if '' != STOCKFISH_PATH and None != STOCKFISH_PATH:
+        engine = chess.engine.SimpleEngine.popen_uci(STOCKFISH_PATH)
+    else :
+        engine = None
+    # Used for testing with small numbers
+    number_of_gambits = 1000
     gambits = []
     for letter in OPENINGS_START:
         openings_page = requests.get(f'{CHESS_OPENINGS_URL}/{letter}.tsv')
@@ -101,8 +115,10 @@ if __name__ == '__main__':
         tsv_reader = csv.reader(openings_lines, delimiter='\t')
         next(tsv_reader)  # Ignore header
         for row in tsv_reader:
+            if(len(gambits) >= number_of_gambits) :
+                break
             if is_gambit(row[1]):
-                analysis = analyse(row[2])
+                analysis = analyse(row[2], row[1], engine)
                 opening_gambit = {
                     'eco': row[0],
                     'name': row[1],
@@ -116,9 +132,11 @@ if __name__ == '__main__':
                     'draws': analysis['draws'],
                     'black': analysis['black'],
                 }
-                print(len(gambits))
+                print(f'{len(gambits)} : {opening_gambit["name"]}')
                 gambits.append(opening_gambit)
-    with open('../js/data/gambits.json', 'w') as json_file:
+        if(len(gambits) >= number_of_gambits) :
+            break
+    with open('gambits.json', 'w') as json_file:
         json.dump(gambits, json_file)
     with open('gambits.csv', 'w', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
@@ -126,3 +144,5 @@ if __name__ == '__main__':
         writer.writerow(keys)
         for gambit in gambits:
             writer.writerow([gambit[key] for key in keys])
+    if engine is not None :
+        engine.close()
