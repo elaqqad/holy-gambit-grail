927f8ae6ba5be695cf8a679c920578bcd6c00da6
Mohamed Elaqqad <elaqqadmohamed@gmail.com>
2023-09-03 00:58:04 +0200

    Add cache for eric rosen and vampirechicken

--- js/utils/caching.ts
+++ js/utils/caching.ts
@@ -2,15 +2,16 @@ import { TrophyCacheFile } from '../types/types'
 
 export async function getCachedGames(url: string): Promise<TrophyCacheFile | undefined> {
     const caches = new Map<string, string>()
-    // caches.set('https://lichess.org/@/zolpi', '/cache/lichess/zolpi.json')
+    caches.set('https://lichess.org/@/zolpi', '/cache/lichess/zolpi.json')
+    caches.set('https://lichess.org/@/ericrosen', '/cache/lichess/ericrosen.json')
     // caches.set('https://lichess.org/@/german11', '/cache/lichess/german11.json')
     // caches.set('https://lichess.org/@/chess-network', '/cache/lichess/chess-network.json')
     // caches.set('https://lichess.org/@/drnykterstein', '/cache/lichess/drnykterstein.json')
-    // caches.set('https://lichess.org/@/ericrosen', '/cache/lichess/ericrosen.json')
     // caches.set('https://lichess.org/@/massterofmayhem', '/cache/lichess/massterofmayhem.json')
     // caches.set('https://lichess.org/@/penguingim1', '/cache/lichess/penguingim1.json')
     // caches.set('https://lichess.org/@/saltyclown', '/cache/lichess/saltyclown.json')
 
+    caches.set('https://www.chess.com/member/vampirechicken', '/cache/chesscom/vampirechicken.json')
     // caches.set('https://www.chess.com/member/alexandrabotez', '/cache/chesscom/alexandrabotez.json')
     // caches.set('https://www.chess.com/member/chessbrah', '/cache/chesscom/chessbrah.json')
     // caches.set('https://www.chess.com/member/danielnaroditsky', '/cache/chesscom/danielnaroditsky.json')
